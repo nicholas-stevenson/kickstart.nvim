93df7e9e1db215fc6fab4228c1edc694d9995bf2
Nicholas Stevenson <nstevenson@bluehole.net>
2023-04-01 10:54:54 -0500

    Adding personal stuffs to my fork of the neovim kikckstart project

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,13 @@
 -- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
 --
--- See the kickstart.nvim README for more information
-return {}
+-- --  I promise not to create any merge conflicts in this directory :)
+--
+-- --
+--
+-- -- See the kickstart.nvim README for more information
+--
+return {
+	{"nicholas-stevenson/sendtomaya.vim", branch="execfile_python39_changes"},
+	"github/copilot.vim"
+}
+
